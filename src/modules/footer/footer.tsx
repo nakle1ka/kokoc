040a6bdf811fbdae9cb166fcbbf87d81305400f0
Artem <artemidqwerty69@gmail.com>
2024-10-04 14:46:37 +0300

    Поменял шрифт

---
+++ src/modules/footer/footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './styles/footer.module.scss'
+
+type Props = {
+
+}
+
+export const Footer: React.FC<Props> = ({}) => {
+    return ( 
+        <div className={styles.test} >
+
+        </div>
+    );
+}
