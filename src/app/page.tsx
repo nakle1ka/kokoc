215952eb571fdb6fc0aefa706021a827c222a31c
Artem <artemidqwerty69@gmail.com>
2024-10-05 22:47:19 +0300

    исправление бага в header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,7 @@
-'use client'
-
-import { MainButton } from "@/components/ui/mainButton/mainButton";
-
 export default function Home() {
   return (
     <div>
-      <MainButton text="d" onClick={() => console.log("test")}/>
+      
     </div>
   );
 }
